c20f97a5d78897a6c6f6d084ee5e26fa8fe03146
Sara Kassem <sara.kasem@azhar.edu.eg>
2017-09-20 15:39:57 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,9 +1,7 @@
 class Movie():
 
-    """ c """
-
+    """ A class to gather movie's title, poster image and YouTube trailer """
     def __init__(self, movie_title, poster_image, trailer_youtube):
-        
             self.title = movie_title
             self.poster_image_url = poster_image
             self.trailer_youtube_url = trailer_youtube
